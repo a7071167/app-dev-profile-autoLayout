2d8f3522ee55a5dde032d071c90bde349cd834cf
Alexander Kufel <kufelalexander@gmail.com>
2018-07-21 09:44:46 +0300

    newProject

---
+++ app-dev-profile-autoLayout/app-dev-profile-autoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-dev-profile-autoLayout
+//
+//  Created by user on 21.07.2018.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
